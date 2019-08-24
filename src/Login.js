7f25959ed5ae06689be4fd98be00ec4aeb511fee
Valentin Barit <valentin.barit@gmail.com>
2019-08-24 17:34:38 +0200

    feat: 🎸 display user infos in popover menu

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,29 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
+import Divider from '@material-ui/core/Divider';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import Grid from '@material-ui/core/Grid';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Popover from '@material-ui/core/Popover';
 import React, { Fragment, useState } from 'react';
+import Typography from '@material-ui/core/Typography';
 
 import useGoogleAuth from './useGoogleAuth';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   avatar: {
-    margin: 10,
+    margin: theme.spacing(1),
   },
-});
+  username: {
+    fontWeight: 700,
+  },
+  userInfo: {
+    padding: theme.spacing(2) * 0.5, // Workaround to avoid overflow due to Grid spacing, cf. https://material-ui.com/fr/components/grid/#marge-n%C3%A9gative
+  },
+}));
 
 export default function ButtonAppBar() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -42,6 +53,7 @@ export default function ButtonAppBar() {
   const currentUser = googleAuth.currentUser.get().getBasicProfile();
   const userName = currentUser.getName();
   const userImageUrl = currentUser.getImageUrl();
+  const userEmailAdress = currentUser.getEmail();
 
   const open = Boolean(anchorEl);
 
@@ -63,25 +75,51 @@ export default function ButtonAppBar() {
         color="inherit"
       >
         <Avatar alt={userName} src={userImageUrl} className={classes.avatar} />
-        <Typography variant="button">{userName}</Typography>
       </Button>
-      <Menu
+      <Popover
         id="menu-appbar"
         anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
         keepMounted
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
         transformOrigin={{
           vertical: 'top',
-          horizontal: 'right',
+          horizontal: 'left',
         }}
         open={open}
         onClose={handleClose}
+        PaperProps={{ square: true }}
+        className={classes.menu}
       >
-        <MenuItem onClick={onLogout}>Logout</MenuItem>
-      </Menu>
+        <div className={classes.userInfo}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <Avatar alt={userName} src={userImageUrl} className={classes.avatar} />
+            </Grid>
+            <Grid item>
+              <Grid container direction="column">
+                <Grid item>
+                  <Typography className={classes.username}>{userName}</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>{userEmailAdress}</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </div>
+        <Divider />
+        <MenuList>
+          <MenuItem onClick={onLogout}>
+            <ListItemIcon>
+              <ExitToAppIcon />
+            </ListItemIcon>
+            <Typography variant="inherit">Logout</Typography>
+          </MenuItem>
+        </MenuList>
+      </Popover>
     </Fragment>
   );
 }
