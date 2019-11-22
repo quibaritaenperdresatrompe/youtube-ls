0e8ab23c5750691cd1b444297581b74fb08e99c3
Valentin Barit <valentin.barit@gmail.com>
2019-11-22 20:35:08 +0100

    feat: 🎸 SecuredRoute is a Route !
    SecuredRoute component use the Route component from react-router

--- src/SecuredRoute.js
+++ src/SecuredRoute.js
@@ -1,12 +1,12 @@
-import { Redirect } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import React, { Suspense } from 'react';
 
 import useGoogleApi from './useGoogleApi';
 
-export default function SecuredRoute({ component: Component, ...props }) {
+export default function SecuredRoute(props) {
   const { isAuthorized } = useGoogleApi();
 
   return (
-    <Suspense fallback={null}>{isAuthorized ? <Component {...props} /> : <Redirect to="/login" />}</Suspense>
+    <Suspense fallback={null}>{isAuthorized ? <Route {...props} /> : <Redirect to="/login" />}</Suspense>
   );
 }
