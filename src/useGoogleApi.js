d6088f1b486f047e1616721f5cf7557cbca5fa07
quibaritaenperdresatrompe <valentin.barit@gmail.com>
2020-02-02 22:50:07 +0100

    refactor: 💡 simplify useGoogleApi hook

--- src/useGoogleApi.js
+++ src/useGoogleApi.js
@@ -3,13 +3,5 @@ import { useContext } from 'react';
 import GoogleApiContext from './GoogleApiContext';
 
 export default function useGoogleApi() {
-  const { auth, client, error, loading, user } = useContext(GoogleApiContext);
-
-  return Object.assign([auth, client, error, loading, user], {
-    auth,
-    client,
-    error,
-    loading,
-    user,
-  });
+  return useContext(GoogleApiContext);
 }
